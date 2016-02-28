6b859d4597c9b6b4515331167b1731a5e282f7cb
Florent THOMAS-MOREL <thomasmorel.florent@gmail.com>
2016-02-28 14:27:14 -0500

    premisse

---
+++ src/routes.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+	return router
+}
+
+var routes = Routes{
+	Route{"Index", "GET", "/", Index},
+}*/
