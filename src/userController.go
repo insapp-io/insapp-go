a3e21a12106827d9601c63ec2cc48f2226b2a5e3
Florent THOMAS-MOREL <thomasmorel.florent@gmail.com>
2016-02-29 10:11:45 -0500

    User Controller

---
+++ src/userController.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/gorilla/mux"
+)
+
+// GetUserController will answer a JSON of the user
+// linked to the given id in the URL
+func GetUserController(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	assocationID := vars["id"]
+	var res = GetUser(bson.ObjectIdHex(assocationID))
+	json.NewEncoder(w).Encode(res)
+}
+
+// AddUserController will answer a JSON of the
+// brand new created user (from the JSON Body)
+func AddUserController(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	decoder.Decode(&user)
+	res := AddUser(user)
+	json.NewEncoder(w).Encode(res)
+}
+
+// UpdateUserController will answer the JSON of the
+// modified user (from the JSON Body)
+func UpdateUserController(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	decoder.Decode(&user)
+	vars := mux.Vars(r)
+	userID := vars["id"]
+	res := UpdateUser(bson.ObjectIdHex(userID), user)
+	json.NewEncoder(w).Encode(res)
+}
+
+// DeleteUserController will answer a JSON of an
+// empty user if the deletation has succeed
+func DeleteUserController(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	res := DeleteUser(bson.ObjectIdHex(vars["id"]))
+	json.NewEncoder(w).Encode(res)
+}
