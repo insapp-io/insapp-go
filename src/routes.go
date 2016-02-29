fe705f75175d35d895f1371babe4acb6c4d7cc32
Florent THOMAS-MOREL <thomasmorel.florent@gmail.com>
2016-02-29 11:17:43 -0500

    Post Model + Post Controller + Routes + Some Fix

--- src/routes.go
+++ src/routes.go
@@ -51,7 +51,19 @@ var routes = Routes{
 	Route{"RemoveParticipant", "DELETE", "/event/{id}/participant/{userID}", RemoveParticipantController},
 
 	//POSTS
+	Route{"GetPost", "GET", "/post/{id}", GetPostController},
+	Route{"GetLastestPost", "GET", "/post", GetLastestPostsController},
+	Route{"AddPost", "POST", "/post", AddPostController},
+	Route{"UpdatePost", "PUT", "/post/{id}", UpdatePostController},
+	Route{"DeletePost", "DELETE", "/post/{id}", DeletePostController},
+	Route{"LikePost", "POST", "/post/{id}/like/{userID}", LikePostController},
+	Route{"DislikePost", "DELETE", "/post/{id}/like/{userID}", DislikePostController},
+	Route{"CommentPost", "POST", "/post/{id}/comment", CommentPostController},
+	Route{"UncommentPost", "DELETE", "/post/{id}/comment", UncommentPostController},
 
 	//USER
-
+	Route{"GetUser", "GET", "/user/{id}", GetUserController},
+	Route{"AddUser", "POST", "/user", AddUserController},
+	Route{"UpdateUser", "PUT", "/user/{id}", UpdateUserController},
+	Route{"DeleteUser", "DELETE", "/user/{id}", DeleteUserController},
 }
