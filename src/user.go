52616c8b96488c0ee979efb33fc20e76d88d2ff5
Florent THOMAS-MOREL <thomasmorel.florent@gmail.com>
2016-02-29 10:05:15 -0500

    User Model

--- src/user.go
+++ src/user.go
@@ -1,17 +1,147 @@
 package main
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
+// User defines how to model a User
 type User struct {
-	ID          bson.ObjectId   `bson:"_id,omitempty"`
-	Name        string          `json:"name"`
-	Username    string          `json:"username"`
-	Description string          `json:"description"`
-	Email       string          `json:"email"`
-	Promotion   string          `json:"promotion"`
-	Events      []bson.ObjectId `json:"events_liked"`
-	PostLiked   []bson.ObjectId `json:"posts_liked"`
-	PhotoURL    string          `json:"photo_url"`
+	ID            bson.ObjectId   `bson:"_id,omitempty"`
+	Name          string          `json:"name"`
+	Username      string          `json:"username"`
+	Description   string          `json:"description"`
+	Email         string          `json:"email"`
+	EmailIsPublic bool            `json:"email_public"`
+	Promotion     string          `json:"promotion"`
+	Events        []bson.ObjectId `json:"events"`
+	PostLiked     []bson.ObjectId `json:"posts_liked"`
+	PhotoURL      string          `json:"photo_url"`
 }
 
+// Users is an array of User
 type Users []User
+
+// AddUser will add the given user from JSON body to the database
+func AddUser(user User) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	db.Insert(user)
+	var result User
+	db.Find(bson.M{"name": user.Username}).One(&result)
+	return result
+}
+
+// UpdateUser will update the user link to the given ID,
+// with the field of the given user, in the database
+func UpdateUser(id bson.ObjectId, user User) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	userID := bson.M{"_id": id}
+	change := bson.M{"$set": bson.M{
+		"name":         user.Name,
+		"description":  user.Description,
+		"email_public": user.EmailIsPublic,
+		"photourl":     user.PhotoURL,
+		"promotion":    user.Promotion,
+	}}
+	db.Update(userID, change)
+	var result User
+	db.Find(bson.M{"_id": id}).One(&result)
+	return result
+}
+
+// DeleteUser will delete the given user from the database
+func DeleteUser(id bson.ObjectId) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	db.RemoveId(id)
+	var result User
+	db.FindId(id).One(result)
+	return result
+}
+
+// GetUser will return an User object from the given ID
+func GetUser(id bson.ObjectId) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	var result User
+	db.FindId(id).One(&result)
+	return result
+}
+
+// LikePost will add the postID to the list of liked post
+// of the user linked to the given id
+func LikePost(id bson.ObjectId, postID bson.ObjectId) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	userID := bson.M{"_id": id}
+	change := bson.M{"$push": bson.M{
+		"posts_liked": postID,
+	}}
+	db.Update(userID, change)
+	var result User
+	db.Find(bson.M{"_id": id}).One(&result)
+	return result
+}
+
+// DislikePost will remove the postID from the list of liked
+// post of the user linked to the given id
+func DislikePost(id bson.ObjectId, postID bson.ObjectId) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	userID := bson.M{"_id": id}
+	change := bson.M{"$pull": bson.M{
+		"posts_liked": postID,
+	}}
+	db.Update(userID, change)
+	var result User
+	db.Find(bson.M{"_id": id}).One(&result)
+	return result
+}
+
+// AddEventToUser will add the eventID to the list
+// of the user's event linked to the given id
+func AddEventToUser(id bson.ObjectId, eventID bson.ObjectId) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	userID := bson.M{"_id": id}
+	change := bson.M{"$push": bson.M{
+		"events": eventID,
+	}}
+	db.Update(userID, change)
+	var result User
+	db.Find(bson.M{"_id": id}).One(&result)
+	return result
+}
+
+// RemoveEventFromUser will remove the eventID from the list
+// of the user's event linked to the given id
+func RemoveEventFromUser(id bson.ObjectId, eventID bson.ObjectId) User {
+	session, _ := mgo.Dial("127.0.0.1")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := session.DB("insapp").C("user")
+	userID := bson.M{"_id": id}
+	change := bson.M{"$pull": bson.M{
+		"events": eventID,
+	}}
+	db.Update(userID, change)
+	var result User
+	db.Find(bson.M{"_id": id}).One(&result)
+	return result
+}
