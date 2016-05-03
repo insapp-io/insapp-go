db63dbfac3fd343348887168fb10a0c239df0738
Florent THOMAS-MOREL <thomasmorel.florent@gmail.com>
2016-05-03 15:05:24 +0200

    CORS

--- src/main.go
+++ src/main.go
@@ -20,12 +20,12 @@ func main() {
 // See: https://groups.google.com/forum/#!topic/golang-nuts/-Sh616lXNRE
 // See: http://stackoverflow.com/a/24818638/1058612.
 func (s *WithCORS) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	if origin := req.Header.Get("Origin"); origin != "" {
-		res.Header().Set("Access-Control-Allow-Origin", "*")
-		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		res.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	}
+	//if origin := req.Header.Get("Origin"); origin != "" {
+	res.Header().Set("Access-Control-Allow-Origin", "*.fthomasmorel.ml")
+	res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	res.Header().Set("Access-Control-Allow-Headers",
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	//}
 
 	// Stop here for a Preflighted OPTIONS request.
 	if req.Method == "OPTIONS" {
